26aa530fa7529d96d27d47b350c8e38c755b039b
Mern-developer <mernstackdeveloper@yahoo.com>
2024-03-02 16:50:45 -0800

    make folder

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -1,7 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
-import AuthProvider from "./AuthProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,9 +13,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-      <AuthProvider>
+      
       <Header />
-      </AuthProvider>
       {children}
       </body>
     </html>
--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,5 +1,4 @@
 "use client";
-import { signIn } from "next-auth/react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
